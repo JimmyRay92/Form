a7f42a09f2b68eafbcc62f217cfbc82cde334ba4
James Wang <jameswang675203@gmail.com>
2023-09-13 01:35:35 +1000

    data

---
+++ src/data/UsageMonthly.ts
@@ -0,0 +1,29 @@
+import arcade from "../assets/icon-arcade.svg";
+import advanced from "../assets/icon-advanced.svg";
+import pro from "../assets/icon-pro.svg";
+
+export const usageMonthly = [
+  {
+    id: "arcade-monthly",
+    img: { src: arcade, alt: "arcade" },
+    heading: "Arcade",
+    pricing: 9,
+    free: "",
+  },
+
+  {
+    id: "advanced-monthly",
+    img: { src: advanced, alt: "advanced" },
+    heading: "Advanced",
+    pricing: 12,
+    free: "",
+  },
+
+  {
+    id: "pro-monthly",
+    img: { src: pro, alt: "pro" },
+    heading: "Pro",
+    pricing: 15,
+    free: "",
+  },
+];
---
+++ src/data/useageYearly.ts
@@ -0,0 +1,29 @@
+import arcade from "../assets/icon-arcade.svg";
+import advanced from "../assets/icon-advanced.svg";
+import pro from "../assets/icon-pro.svg";
+
+export const usageYearly = [
+  {
+    id: "arcade-yearly",
+    img: { src: arcade, alt: "arcade" },
+    heading: "Arcade",
+    pricing: 90,
+    free: "2 months free",
+  },
+
+  {
+    id: "advanced-yearly",
+    img: { src: advanced, alt: "advanced" },
+    heading: "Advanced",
+    pricing: 120,
+    free: "2 months free",
+  },
+
+  {
+    id: "pro-yearly",
+    img: { src: pro, alt: "pro" },
+    heading: "Pro",
+    pricing: 150,
+    free: "2 months free",
+  },
+];
